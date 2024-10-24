7089dc66492ee20819d372d109cb74958134b6f7
Bayane Chakir <bayane.pbg@gmail.com>
2024-10-24 16:49:24 +0200

    ex 4 done, ex 5 is in progress

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,18 @@
-const removeFromArray = function(firstArgument, secondArgument) {
-    const numValue = [firstArgument];
-    numValue.pop(secondArgument);
-    return numValue;
 
+// the first arg. is about the whole array and the second is about the values to remove from the array.
+function removeFromArray(firstArray, ...values) { 
 
+    values.forEach((value) => { // the forEach loop take the value out from the array to remove it
+        let position = firstArray.indexOf(value); //read the position of the value to take out
+
+        while (position >= 0) { // adding a while loop is to make sure there's not other value left out 
+            firstArray.splice(position, 1);
+            position = firstArray.indexOf(value);// read the position again and try to make sure the position is set to -1 to go out from the loop 
+        }
+
+    });
+
+    return firstArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function (firstNum, secondNum) {
 
-};
+    let checkTypeOfFirst = typeof firstNum; 
+    let checkTypeOfSecond = typeof secondNum; 
+
+    if (checkTypeOfFirst !== "number" && checkTypeOfSecond !== "number"){
+        return "ERROR";
+    } else {
+        let arrayBetween = [];
+        let total = 0;
+        for (let i = firstNum; i <= secondNum; i++) {
+            arrayBetween.push(i);
+            total = total + i;
+
+        }
+        return total;
+    }
+
+
+
+ };
 
 // Do not edit below this line
 module.exports = sumAll;
