4bfa4513a64787c2df1a86e4481ce9e4f1c6e630
Bayane Chakir <bayane.pbg@gmail.com>
2024-10-25 16:58:36 +0200

    adding 3 exercices

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,24 +1,46 @@
+
+
 const sumAll = function (firstNum, secondNum) {
 
-    let checkTypeOfFirst = typeof firstNum; 
-    let checkTypeOfSecond = typeof secondNum; 
-
-    if (checkTypeOfFirst !== "number" && checkTypeOfSecond !== "number"){
+    // error if the argument isn't a number 
+    if (typeof firstNum !== "number" || typeof secondNum !== "number") {
         return "ERROR";
-    } else {
-        let arrayBetween = [];
-        let total = 0;
-        for (let i = firstNum; i <= secondNum; i++) {
-            arrayBetween.push(i);
-            total = total + i;
-
-        }
-        return total;
     }
 
+    // if first number is bigger that second, invert numbers 
+    if (firstNum > secondNum) {
+        let initialFirstNum = firstNum;
+        firstNum = secondNum;
+        secondNum = initialFirstNum;
+    }
+
+    // error if one of the arguments is negative
+    if (firstNum < 0 || secondNum < 0) {
+        return "ERROR";
+    }
+
+    // error if modulo isn't 0 (if number is float)
+    const isIntFirstNum = firstNum % 1
+    const isIntSecondNum = secondNum % 1
+    if (isIntFirstNum !== 0 || isIntSecondNum !== 0) {
+        return "ERROR";
+    }
+
+    // and if no error it gives the sum within the range
+    let total = 0;
+    for (let i = firstNum; i <= secondNum; i++) {
+        total += i;
+    }
+
+    return total;
+
+};
+
+// function isNumber(val) { // function to make sure it's a number
+//     return typeof val === "number";
+// }
 
 
- };
 
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,42 @@
-const convertToCelsius = function() {
+
+
+
+
+
+const convertToCelsius = function(temperatureF) {
+
+  //formula to convert fahren. to cels.
+
+  let celsiusTemp = ((temperatureF - 32) * 5)/9
+
+  // round numbers to one decimal
+  const roundTempF = (Math.round(celsiusTemp * 10) / 10)
+                                                                                           
+  return roundTempF;
+  
+};
+  
+
+const convertToFahrenheit = function(temperatureC) {
+  
+  //formula to convert Cels. to fahren.
+  let fahrenheitTemp = (temperatureC * (9/5)) + 32; 
+
+  // round numbers to one decimal
+  const roundTempC = (Math.round(fahrenheitTemp * 10) / 10)
+
+  return roundTempC;
+  
 };
 
-const convertToFahrenheit = function() {
-};
+
+
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = {
